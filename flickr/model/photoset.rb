abc1cbc96d1cf035aaf89a068282a5187e95759d
Alistair Young <alistair@codebrane.com>
2017-08-20 14:24:34 +0100

    Add to_json for creating metadata file

--- flickr/model/photoset.rb
+++ flickr/model/photoset.rb
@@ -1,5 +1,22 @@
 class PhotoSet < FlickrObject
   attr_accessor :id, :primary, :secret, :server, :farm, :photos, :videos,
                 :title, :description, :count_views, :count_comments,
-                :date_create, :date_update
+                :date_create, :date_update, :ownername
+                
+  def to_json(*args)
+  {
+    'id' => @id,
+    'primary' => @primary,
+    'secret' => @secret,
+    'farm' => @farm,
+    'title' => @title,
+    'description' => @description,
+    'noOfViews' => @count_views,
+    'noOfComments' => @count_comments,
+    'ownername' => @ownername,
+    'dateCreated' => human_date(@date_create),
+#    'dateUpdated' => (@date_update == "0") ? nil : human_date(@date_update),
+    'dateUpdated' => human_date(@date_update)
+  }.to_json(*args)
+  end
 end
