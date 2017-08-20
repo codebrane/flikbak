86407e947f61330b1b860094ac58f7f08bd875d7
Alistair Young <alistair@codebrane.com>
2017-08-20 14:23:47 +0100

    Parse non unix style dates

--- flickr/model/FlickrObject.rb
+++ flickr/model/FlickrObject.rb
@@ -4,4 +4,8 @@ class FlickrObject
   def human_date(date)
     DateTime.strptime(date, '%s').strftime("%d/%m/%Y %H:%M:%S")
   end
+  
+  def human_date_from_string(date_string)
+    DateTime.parse(date_string).strftime("%d/%m/%Y %H:%M:%S")
+  end
 end
