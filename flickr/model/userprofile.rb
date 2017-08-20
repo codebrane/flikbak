079f6f27eee5c528d2947a69cbca158bd5f47aab
Alistair Young <alistair@codebrane.com>
2017-08-20 14:25:16 +0100

    Represents information about a commenter

---
+++ flickr/model/userprofile.rb
@@ -0,0 +1,23 @@
+class UserProfile < FlickrObject
+  attr_accessor :join_date, :occupation, :hometown, :showcase_set,
+                :profile_description, :facebook, :twitter, :tumblr,
+                :instagram, :pinterest, :id, :nsid, :realname
+                
+  def to_json(*args)
+  {
+    'id' => @id,
+    'nsid' => @nsid,
+    'realName' => @realname,
+    'joinDate' => human_date(@join_date),
+    'occupation' => @occupation,
+    'hometown' => @hometown,
+    'showcase_set' => @showcase_set,
+    'profile_description' => @profile_description,
+    'facebook' => @facebook,
+    'twitter' => @twitter,
+    'tumblr' => @tumblr,
+    'instagram' => @instagram,
+    'pinterest' => @pinterest
+  }.to_json(*args)
+  end
+end
