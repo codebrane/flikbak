db27a6a583ee32cdee54e34777f00a92f1f04c60
Alistair Young <alistair@codebrane.com>
2017-08-21 11:13:12 +0100

    Implement collections

--- flickbak.rb
+++ flickbak.rb
@@ -37,9 +37,40 @@ p "created in #{photos_dir}/groups.json"
 
 if (mode == 'collections')
   p "Looking for your collections"
+  
+  collections_dir = "#{photos_dir}/collections"
+  Dir.mkdir(collections_dir) unless File.exists?(collections_dir)
+  
   collections = flickr.get_collections(user.id)
   collections.each do |collection|
     p "#{collection.title} -> #{collection.sets.count}"
+    
+    collection_title_for_disk = "#{collection.title.downcase.gsub(/[\s,\/&.']/, "_")}-#{collection.id}"
+    collection_dir = "#{collections_dir}/#{collection_title_for_disk}"
+    Dir.mkdir(collection_dir) unless File.exists?(collection_dir)
+    
+    if (collection.sets.count > 0)
+      photosets_count = 0
+      collection.sets.each do |photoset|
+        photosets_count += 1
+        p "Photoset #{photosets_count}/#{collection.sets.count} #{photoset.title}"
+        photoset_dir = "#{collection_dir}/#{photoset.title.gsub(/[\s,\/&.]/, "_")}"
+        Dir.mkdir(photoset_dir) unless File.exists?(photoset_dir)
+        photos = flickr.get_photos_in_photoset(user.id, photoset)
+        photos_count = 0
+        photos.each do |photo|
+          photos_count += 1
+          p "Photo #{photos_count}/#{photos.count} #{photo.title}"
+          photo.original_url = flickr.get_original_photo_url(photo)
+          photo_title_for_disk = "#{photo.title.downcase.gsub(/[\s,\/&]/, "_")}-#{photo.id}"
+          photo_dir = "#{photoset_dir}/#{photo_title_for_disk}"
+          Dir.mkdir(photo_dir) unless File.exists?(photo_dir)
+#          flickr.download_photo(photo.original_url, "#{photo_dir}/#{photo_title_for_disk}.#{photo.originalformat}")
+          flickr.create_metadata_file(photo, "#{photo_dir}/#{photo_title_for_disk}.json")
+        end
+        flickr.create_metadata_file(photoset, "#{photoset_dir}/#{photoset.title.gsub(/[\s,\/]/, "_")}.json")
+      end
+    end
   end
   exit
 end
@@ -52,7 +83,7 @@ if (mode == 'notinset')
     photos_count += 1
     p "Photo not in set #{photos_count}/#{photos_not_in_sets.count} #{photo.title}"
     photo.original_url = flickr.get_original_photo_url(photo)
-    photo_title_for_disk = "#{photo.title.downcase.gsub(/[\s,&]/, "_")}-#{photo.id}"
+    photo_title_for_disk = "#{photo.title.downcase.gsub(/[\s,\/&]/, "_")}-#{photo.id}"
     not_in_set_dir = "#{photos_dir}/not_in_set"
     Dir.mkdir(not_in_set_dir) unless File.exists?(not_in_set_dir)
     photo_dir = "#{not_in_set_dir}/#{photo_title_for_disk}"
@@ -77,7 +108,7 @@ if (mode == 'sets')
       photos_count += 1
       p "Photo #{photos_count}/#{photos.count} #{photo.title}"
       photo.original_url = flickr.get_original_photo_url(photo)
-      photo_title_for_disk = "#{photo.title.downcase.gsub(/[\s,&]/, "_")}-#{photo.id}"
+      photo_title_for_disk = "#{photo.title.downcase.gsub(/[\s,\/&]/, "_")}-#{photo.id}"
       photo_dir = "#{photoset_dir}/#{photo_title_for_disk}"
       Dir.mkdir(photo_dir) unless File.exists?(photo_dir)
       flickr.download_photo(photo.original_url, "#{photo_dir}/#{photo_title_for_disk}.#{photo.originalformat}")
