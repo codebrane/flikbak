75a080f979274f036011c12691cf66eda68fef78
Alistair Young <alistair@codebrane.com>
2017-08-21 11:13:01 +0100

    Sanity checking

--- flickr/model/FlickrObject.rb
+++ flickr/model/FlickrObject.rb
@@ -2,10 +2,10 @@ require 'date'
 
 class FlickrObject
   def human_date(date)
-    DateTime.strptime(date, '%s').strftime("%d/%m/%Y %H:%M:%S")
+    DateTime.strptime(date, '%s').strftime("%d/%m/%Y %H:%M:%S") unless date.nil?
   end
   
   def human_date_from_string(date_string)
-    DateTime.parse(date_string).strftime("%d/%m/%Y %H:%M:%S")
+    DateTime.parse(date_string).strftime("%d/%m/%Y %H:%M:%S") unless date_string.nil?
   end
 end
